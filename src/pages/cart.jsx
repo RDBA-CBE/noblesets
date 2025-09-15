b4e128f34074464cf9fbf2870bfd57662331a7fe
nsathiyakala <nsathiyakala6@gmail.com>
2025-09-15 17:17:11 +0530

    build issue

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -6,7 +6,7 @@ import CartArea from '@/components/cart-wishlist/cart-area';
 import CommonBreadcrumb from '@/components/breadcrumb/common-breadcrumb';
 import FooterTwo from '@/layout/footers/footer-2';
 import { useGetCartListQuery } from '@/redux/features/card/cardApi';
-import CartBanner from "../../public/assets/img/header-bg.web";
+import CartBanner from "../../public/assets/img/header-bg.webp";
 import HeaderSection from '@/components/home/headerSection';
 import CartArea1 from '@/components/cart-wishlist/cart-area1';
 import HomeFooter from "@/components/home/HomeFooter";
