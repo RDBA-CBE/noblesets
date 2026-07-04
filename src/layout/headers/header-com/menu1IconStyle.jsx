3ea8d0b7fcf21960e7df68e508d6d362d07336df
Sathiya kala <nsathiyakala6@gmail.com>
2026-07-04 18:07:59 +0530

    Update icon paths for bracelets and earrings

--- src/layout/headers/header-com/menu1IconStyle.jsx
+++ src/layout/headers/header-com/menu1IconStyle.jsx
@@ -39,10 +39,10 @@ const SUB_CATEGORY_IMAGES = {
   "earring": "/assets/img/navbar/Gold Earrings.svg",
   "rings": "/assets/img/navbar/Gold Ring.svg",
   "necklace": "/assets/img/navbar/Gold Ring-1.svg",
-  "bracelets": "/assets/img/navbar/D - pendant.svg",
-
+  "bracelets": "/assets/img/navbar/Gold Bangle.svg",
+  "earrings-2": "/assets/img/navbar/Gold Earrings.svg",
   "bangle-1": "/assets/img/navbar/Gold Bangle.svg",
-  "earring-": "/assets/img/navbar/Gold Earrings.svg",
+  "earrings-1": "/assets/img/navbar/Gold Earrings.svg",
   "ring-1": "/assets/img/navbar/Gold Ring.svg",
   "necklace-1": "/assets/img/navbar/Gold Ring-1.svg",
   "bracelet-1": "/assets/img/navbar/Gold Bangle.svg",
